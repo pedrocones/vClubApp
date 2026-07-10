0d817b2446798e337d8fe9cc6bc6f8c9ac39cc4e
PedroCones <pedrocones@yahoo.com>
2026-07-10 12:14:42 -0700

    last know working including emulator debug - startd refactoring for go_router+main_layout+provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:flutter/foundation.dart';
+// ignore: unused_import
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'hmi/pages/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // ONLY connect to emulators if you are running a local debug build
+  if (kDebugMode) {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
+  }
+
   runApp(const MyApp());
 }
+
+/* void main() {
+  runApp(const MyApp());
+} */
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
